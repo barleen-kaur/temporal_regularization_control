4f0f43824f1b5b79d1088a0d88ebeaefa6f5ee45
Raviteja Chunduru <ravi.tej.310@gmail.com>
2019-07-12 16:57:57 -0400

    Updated run time to 10 hours

--- scripts/dqn_reg.sh
+++ scripts/dqn_reg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=07:00:00
+#SBATCH --time=10:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --ntasks=3
+#SBATCH --ntasks=5
 #SBATCH --mem-per-cpu=8GB
 #SBATCH --account=rpp-bengioy
 #SBATCH --array=1-5 
